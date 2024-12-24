79e289dbd4abb78ac9df5a52e5a0efe1a446e12c
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-12-24 17:24:46 +0100

    Setup client side to send signature to server

--- client/src/Transfer.jsx
+++ client/src/Transfer.jsx
@@ -1,54 +1,65 @@
-import { useState } from "react";
-import server from "./server";
+import {useState} from 'react';
+import {bls12_381 as bls} from 'ethereum-cryptography/bls.js';
+import {toHex, utf8ToBytes} from 'ethereum-cryptography/utils.js';
+import server from './server';
 
-function Transfer({ address, setBalance }) {
-  const [sendAmount, setSendAmount] = useState("");
-  const [recipient, setRecipient] = useState("");
+function Transfer({address, setBalance, privateKey}) {
+    const [sendAmount, setSendAmount] = useState('');
+    const [recipient, setRecipient] = useState('');
 
-  const setValue = (setter) => (evt) => setter(evt.target.value);
+    const setValue = (setter) => (evt) => setter(evt.target.value);
 
-  async function transfer(evt) {
-    evt.preventDefault();
+    async function transfer(evt) {
+        evt.preventDefault();
 
-    try {
-      const {
-        data: { balance },
-      } = await server.post(`send`, {
-        sender: address,
-        amount: parseInt(sendAmount),
-        recipient,
-      });
-      setBalance(balance);
-    } catch (ex) {
-      alert(ex.response.data.message);
+        const message = utf8ToBytes(
+            JSON.stringify({
+                address,
+                sendAmount,
+                recipient,
+            })
+        );
+        const signature = bls.sign(message, privateKey);
+        try {
+            const {
+                data: {balance},
+            } = await server.post(`send`, {
+                sender: address,
+                amount: parseInt(sendAmount),
+                recipient,
+                message: Array.from(message),
+                privateKey,
+                signature: Array.from(signature),
+            });
+            setBalance(balance);
+        } catch (ex) {
+            alert(ex.response.data.message);
+        }
     }
-  }
 
-  return (
-    <form className="container transfer" onSubmit={transfer}>
-      <h1>Send Transaction</h1>
+    return (
+        <form className="container transfer" onSubmit={transfer}>
+            <h1>Send Transaction</h1>
 
-      <label>
-        Send Amount
-        <input
-          placeholder="1, 2, 3..."
-          value={sendAmount}
-          onChange={setValue(setSendAmount)}
-        ></input>
-      </label>
+            <label>
+                Send Amount
+                <input
+                    placeholder="1, 2, 3..."
+                    value={sendAmount}
+                    onChange={setValue(setSendAmount)}></input>
+            </label>
 
-      <label>
-        Recipient
-        <input
-          placeholder="Type an address, for example: 0x2"
-          value={recipient}
-          onChange={setValue(setRecipient)}
-        ></input>
-      </label>
+            <label>
+                Recipient
+                <input
+                    placeholder="Type an address, for example: 0x2"
+                    value={recipient}
+                    onChange={setValue(setRecipient)}></input>
+            </label>
 
-      <input type="submit" className="button" value="Transfer" />
-    </form>
-  );
+            <input type="submit" className="button" value="Transfer" />
+        </form>
+    );
 }
 
 export default Transfer;
